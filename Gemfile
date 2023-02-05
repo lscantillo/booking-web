ec8fa0617a2d38e4f3029db64284d1ce40f583ff
lscantillo <luissebastiancantillo@gmail.com>
2023-02-05 16:17:56 -0500

    Add aws s3 sdk

--- Gemfile
+++ Gemfile
@@ -82,4 +82,4 @@ gem "friendly_id", "~> 5.5"
 
 gem "dockerfile-rails", ">= 1.0.0", :group => :development
 
-gem "aws-sdk-s3", "~> 1.119"
+gem "aws-sdk-s3", "~> 1.119", require: false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
   config.active_storage.variant_processor = :mini_magick
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -35,7 +35,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :digitalocean
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
