c06f02e8d1037fc4782bb9cf901ea3765fa00737
lscantillo <luissebastiancantillo@gmail.com>
2023-02-05 20:43:57 -0500

    Render build and refactor update folder

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,17 @@
+Rails.configuration.to_prepare do
+  ActiveStorage::Blob.class_eval do
+    before_create :generate_key_with_prefix
+
+    def generate_key_with_prefix
+      self.key = if prefix
+                   File.join prefix, self.class.generate_unique_secure_token
+                 else
+                   self.class.generate_unique_secure_token
+                 end
+    end
+
+    def prefix
+      Rails.application.credentials.dig(:active_storage, :prefix) + '/' + Rails.env
+    end
+  end
+end
